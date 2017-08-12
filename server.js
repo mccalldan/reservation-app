68de1149e8d894c61ce9ba344bf4f518d5739085
quinton jones <quinton.jones714@gmail.com>
2017-08-12 10:53:06 -0400

    new

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-ar http = require("http");
-
+var http = require("http");
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
 // Here we define a port to listen to
 var PORT = 8080;
 
-// Here we create a generic function to handle requests and responses
-function handleRequest(request, response) {
+var app = express();
 
-  // The below statement is triggered (client-side) when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Here we use the Node HTTP package to create our server.
-// We then pass it the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+app.listen(PORT, function() {
 
-// Here we start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+ console.log("App listening on PORT " + PORT);
 
-  // The below statement is triggered (server-side) when a user visits the PORT URL
-  console.log("Server listening on: http://localhost:%s", PORT);
+});
 
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Get all characters
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
